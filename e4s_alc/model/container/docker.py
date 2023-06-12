34ed184e7e066135fdb6162201024069dab13446
Frederick Deny <denyfrederick@gmail.com>
2023-06-12 15:33:21 -0700

    revamped logging for docker controller

--- e4s_alc/model/container/docker.py
+++ e4s_alc/model/container/docker.py
@@ -5,6 +5,9 @@ from prettytable import PrettyTable
 from dateutil import parser
 import requests
 from e4s_alc.mvc.controller import Controller
+from e4s_alc import logger
+
+LOGGER = logger.get_logger(__name__)
 
 has_docker=False
 try:
@@ -26,27 +29,26 @@ class DockerController(Controller):
 
         # Try to import the python library
         if not has_docker:
-            print('Failed to find Docker python library')
+            LOGGER.debug('Failed to find Docker python library')
             return
 
         # Try to connect with the docker runtime
         try:
             self.client = docker.from_env(timeout=600)
         except docker.errors.DockerException:
-            print('Failed to connect to Docker client')
+            LOGGER.debug('Failed to connect to Docker client')
             return
         
         self.is_active = True
 
     def pull_image(self, image):
-        import docker
         self.image = image
 
         # Parse the image and tag
         if ':' in self.image:
             image_chunks = self.image.split(':')
             if len(image_chunks) != 2:
-                print('Error processing image \'{}\'.'.format(self.image))
+                LOGGER.warning('Error processing image \'{}\'.'.format(self.image))
             self.image_os, self.image_tag = image_chunks
         else:
             self.image_os, self.image_tag = self.image, 'latest'
@@ -55,21 +57,20 @@ class DockerController(Controller):
         try:
             self.client.images.pull(self.image_os, self.image_tag)
         except docker.errors.ImageNotFound:
-            print('Image was not found.')
+            LOGGER.warning('Image was not found.')
             exit(1)
         except docker.errors.NotFound:
-            print('Image was not found.')
+            LOGGER.warning('Image was not found.')
             exit(1)
 
     def find_image(self, image):
-        import docker
         self.image = image
 
         # Try to get image from client
         try:
             self.client.images.get(image)
         except docker.errors.ImageNotFound:
-            print('Image was not found.')
+            LOGGER.warning('Image was not found.')
             exit(1)
 
 
@@ -149,7 +150,7 @@ class DockerController(Controller):
                 print()
 
         except:
-            print('Stopping container...')
+            LOGGER.info('Stopping container...')
             container.stop()
             exit(1)
 
@@ -160,12 +161,11 @@ class DockerController(Controller):
         container.stop()
 
     def list_images(self, name=None, inter=False):
-        import docker
         try:
             images = self.client.images.list(name, all=inter)
         except docker.errors.APIError as err:
             error_string = "Image listing has failed:"
-            print(error_string)
+            LOGGER.error(error_string)
             raise SystemExit(err) from err
         self.show_images(images)
 
@@ -182,7 +182,6 @@ class DockerController(Controller):
         print(t)
     
     def prune_images(self):
-        import docker
         entered_value = input("WARNING: All dangling images will be deleted, are you sure you want to proceed?[y/N]\n")
         if entered_value in ['y', 'Y', 'yes']:
             try:
@@ -190,17 +189,15 @@ class DockerController(Controller):
             except docker.errors.APIError as err:
                 raise SystemExit(err) from err
             if not deleted["ImagesDeleted"]:
-                print("No images were deleted: no unused images found.\nAre the corresponding stopped containers removed?\nConsider using 'e4s-alc delete -c $CONTAINER_ID' or 'e4s-alc delete --prune-containers'.")
+                LOGGER.info("No images were deleted: no unused images found.\nAre the corresponding stopped containers removed?\nConsider using 'e4s-alc delete -c $CONTAINER_ID' or 'e4s-alc delete --prune-containers'.")
             else:
-                self.print_line()
-                print("Pruned images:\n")
+                LOGGER.info("Pruned images:\n")
                 for item in deleted['ImagesDeleted']:
-                    print(item['Deleted'])
-                print("\nSpace Reclaimed:\n")
-                print(human_readable_size(deleted['SpaceReclaimed']))
+                    LOGGER.info(item['Deleted'])
+                LOGGER.info("\nSpace Reclaimed:\n")
+                LOGGER.info(human_readable_size(deleted['SpaceReclaimed']))
 
     def prune_containers(self):
-        import docker
         entered_value = input("WARNING: All stopped containers will be deleted, are you sure you want to proceed?[y/N]\n")
         if entered_value in ['y', 'Y', 'yes']:
             try:
@@ -208,15 +205,13 @@ class DockerController(Controller):
             except docker.errors.APIError as err:
                 raise SystemExit(err) from err
             if not deleted["ContainersDeleted"]:
-                print("No containers were deleted: no stopped containers found.")
+                LOGGER.info("No containers were deleted: no stopped containers found.")
             else:
-                self.print_line()
-                print("Pruned containers:\n")
+                LOGGER.info("Pruned containers:\n")
                 for item in deleted['ContainersDeleted']:
-                    print(item)
-                print("\nSpace Reclaimed:")
-                print(human_readable_size(deleted['SpaceReclaimed'], 2))
-                self.print_line()
+                    LOGGER.info(item)
+                LOGGER.info("\nSpace Reclaimed:")
+                LOGGER.info(human_readable_size(deleted['SpaceReclaimed'], 2))
 
     def delete_image(self, names, force):
         try:
@@ -229,15 +224,14 @@ class DockerController(Controller):
                 error_string += " image not found with name {}.".format(name)
             elif 409:
                 error_string += " image {} used by container. Use '-f' to force remove, or remove container using 'alc delete -c $CONTAINER_ID'.".format(name)
-            print(error_string)
+            LOGGER.error(error_string)
             raise SystemExit(err) from err
 
     def delete_container(self, ID, force):
-        import docker
         try:
             current = self.client.containers.get(ID)
             current.remove(force=force)
         except docker.errors.APIError as err:
             error_string = "Container deletion has failed:"
-            print(error_string)
+            LOGGER.error(error_string)
             raise SystemExit(err) from err
