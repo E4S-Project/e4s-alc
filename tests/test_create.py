ce434e680787c98aa3a91aecfef8face701ff90c
Frederick Deny <denyfrederick@gmail.com>
2023-06-27 15:15:59 -0700

    expanded test on docker to singularity

--- tests/test_create.py
+++ tests/test_create.py
@@ -217,3 +217,46 @@ class SingularityCreateTests(unittest.TestCase):
         controller.install_spack()
         self.assertIsNone(controller.execute_build("unittesting"))
         controller.delete_image(['unittesting.sif'], True)
+
+    @unittest.skipIf('spython' not in sys.modules, "Singularity not available")
+    def test_find_image(self):
+        controller = SingularityController()
+        controller_2 = SingularityController()
+        controller.pull_image('ubuntu')
+        controller_2.find_image("ubuntu")
+        self.assertEqual(controller_2.image, "ubuntu")
+        controller.parent.delete_image(controller, ['ubuntu'], True)
+        with self.assertRaises(SystemExit) as system_exit:
+            controller_2.find_image("ubuntuifkajsh")
+
+    @unittest.skipIf('spython' not in sys.modules, "Singularity not available")
+    def test_pull_image(self):
+        controller = SingularityController()
+        self.assertIsNone(controller.pull_image('ubuntu'))
+        controller.parent.delete_image(controller, ['ubuntu'], True)
+        with self.assertRaises(SystemExit) as system_exit:
+            controller.parent.delete_image(controller, ['ubuntudsg'], True)
+
+    @unittest.skipIf('spython' not in sys.modules, "Singularity not available")
+    def test_delete_image(self):
+        controller = SingularityController()
+        controller.pull_image('ubuntu')
+        with self.assertRaises(SystemExit) as system_exit:
+            controller.parent.delete_image(controller, ['ubuntudsg'], True)
+        self.assertIsNone(controller.parent.delete_image(controller, ['ubuntu'], True))
+
+    @unittest.skipIf('spython' not in sys.modules, "Singularity not available")
+    def test_parse_os_release(self):
+        controller = SingularityController()
+        controller.pull_image('ubuntu:18.04')
+        controller.parse_os_release()
+        self.assertEqual(controller.os_release['PRETTY_NAME'], 'Ubuntu 18.04.6 LTS')
+        controller.parent.delete_image(controller, ['ubuntu:18.04'], True)
+
+    @unittest.skipIf('spython' not in sys.modules, "Singularity not available")
+    def test_parse_environment(self):
+        controller = SingularityController()
+        controller.pull_image('ubuntu:18.04')
+        controller.parse_environment()
+        self.assertEqual(controller.environment['PATH'], '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin')
+        controller.parent.delete_image(controller, ['ubuntu:18.04'], True)
